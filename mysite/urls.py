03037da8a4f5090392635ea421fa14974d870d19
h-yes-oo <hyesoo5115@naver.com>
2022-10-02 01:10:07 +0900

    feat: bootstrap 을 활용한 홈 화면 템플릿 추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mysite.views import HomeView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
 ]
