a9094a4a773693b0b6b5cd7084c0abadad812e58
h-yes-oo <hyesoo5115@naver.com>
2022-10-09 16:37:29 +0900

    feat: api2 앱 추가 및 설정

--- api2/urls.py
+++ api2/urls.py
@@ -1,14 +1,16 @@
-from django.contrib import admin
+# Routers provide an easy way of automatically determining the URL conf.
 from django.urls import path, include
+from rest_framework import routers
+
+from api2.views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('api2/', include('api2.urls')),
-
-    # path('', include(router.urls)),
+    path('', include(router.urls)),
     # 아래 줄은 단순히 화면에 로그인 버튼을 보여주는 역할을 할 뿐
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
